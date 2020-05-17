52a7fd40db0c700a334261c1b96f77b0dcdd298d
João Barbosa <64969275+joao-albc@users.noreply.github.com>
2020-05-17 00:01:59 -0300

    Inserção do exercício 91
    Vamos ver se isso vai funcionar

--- Python/Exercícios/exercicio091.py
+++ Python/Exercícios/exercicio091.py
@@ -1,1 +1,16 @@
-Fazendo o teste do exercício
+from datetime import date 
+
+hoje = date.today().year
+
+pessoa = {}
+
+while True:
+    pessoa['nome'] = str(input('Nome:'))
+    pessoa['nascimento'] = int(input('Ano de nascimento: '))
+    pessoa['ctps'] = int(input('Nº da carteira de trabalho: '))
+    if pessoa['ctps'] == 0:
+        break
+    pessoa['salario'] = int(input('Salário'))
+    pessoa['idade'] = hoje - (pessoa['nascimento']) 
+
+print (pessoa)
