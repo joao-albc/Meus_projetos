33675133a1feb7ad28b83c05c1582aff18899470
João Barbosa <joao.albc@gmail.com>
2020-05-21 01:03:06 -0300

    Update exercicio093.py
    partidas =  int(input(f'Quantas partidas {cadastro['nome']} jogou?'))
    
    Não estava funcionando

--- Python/Exercícios/exercicio093.py
+++ Python/Exercícios/exercicio093.py
@@ -1,5 +1,19 @@
-jogador = {'nome':str(input('Nome do jogador(a): '))}
-for v in jogador:
-    partidas =  int(input(f'Quantas partidas {(jogador[v])} jogou?'))
+cadastro = {}
+
+nome = {str(input('Nome do jogador(a): '))}
+partidas =  int(input(f'Quantas partidas {nome} jogou?'))
 gols =  {}
-total = {}
+gols_marcados = []
+for c in range(1,partidas + 1):
+    valor = int(input(f'Quantos gols na partidas {(c)}? '))
+    gols_marcados.append(valor)
+print ('-='*10)
+gols = gols_marcados
+#print (gols)
+total = {'total':sum(gols_marcados)}
+#print (total)
+cadastro['nome'] = nome
+cadastro['gols'] = gols_marcados
+cadastro['total'] = total
+
+print (cadastro)
