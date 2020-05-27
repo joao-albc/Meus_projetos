97a16d81fbe9f2df8297c94df05be1cc433a0ba3
João Barbosa <joao.albc@gmail.com>
2020-05-26 22:11:18 -0300

    Update exercicio095.py

--- Python/Exercícios/exercicio095.py
+++ Python/Exercícios/exercicio095.py
@@ -1,19 +1,45 @@
 #Aprimorar o exercício 93
 
+cadastros = []
 cadastro = {}
+gols_marcados = list()
+while True:
+    cadastro.clear
+    
+    cadastro['nome'] = {str(input('Nome do jogador(a): '))}
+    cadastro['partidas'] =  int(input(f'Quantas partidas {(cadastro["nome"])} jogou? '))
 
-cadastro['nome'] = {str(input('Nome do jogador(a): '))}
-cadastro['partidas'] =  int(input(f'Quantas partidas {(cadastro["nome"])} jogou? '))
-gols_marcados = []
 
-for c in range(1,cadastro['partidas'] + 1):
-    valor = int(input(f'Quantos gols na partidas {(c)}? '))
-    gols_marcados.append(valor)
-print ('-='*10)
+    for c in range(1,cadastro['partidas'] + 1):
+        valor = int(input(f'Quantos gols na partidas {(c)}? '))
+        gols_marcados.append(valor)
+    print ('-='*10)
 
-cadastro['gols'] = gols_marcados
-cadastro['total'] = sum(gols_marcados)
+    cadastro['gols'] = gols_marcados
+    #ARRUMAR OS GOLS MARCADOS
+    cadastro['total'] = sum(gols_marcados)
 
+    cadastros.append(cadastro.copy())
+    gols_marcados.clear
+    #Continuar? Sim ou Não é preciso corrigir essa parte
+    
+    while True:
+        continuar = str(input('Deseja continuar? [S/N]')).upper()[0]
+        if continuar not in 'NnSs':
+            print('Por favor, digitar S ou N.') 
+        elif continuar in 'Ss':
+            print()
+            print (f'{"="*4}Novo Cadastro{"="*4}')
+            print()
+            break
+        if continuar in 'Nn':
+            break
+    if continuar in 'Nn':
+        print (f'{"="*4}Cadastro Finalizado{"="*4}')
+        break
+print (cadastros)
+
+''''
 print (cadastro)
 print ('-='*10)
 
@@ -25,4 +51,4 @@ print ('-='*10)
 print(f'O jogador(a) {cadastro["nome"]} jogou {cadastro["partidas"]}')
 
 for k, v in enumerate (gols_marcados):
-     print(f'Na partida {k}, fez {v} gols')
+     print(f'Na partida {k}, fez {v} gols')'''
