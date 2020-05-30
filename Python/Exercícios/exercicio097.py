1fd12eb8d81b3fd16a11ebf68b7c58686682188f
João Barbosa <joao.albc@gmail.com>
2020-05-30 17:33:07 -0300

    Exercícios de função

---
+++ Python/Exercícios/exercicio097.py
@@ -0,0 +1,14 @@
+def escreva(txt):
+    print (f'{len(txt)*"~"}')
+    print (txt)
+    print (f'{len(txt)*"~"}')
+'''
+Este exemplo debaixo tem o conteúdo em aberto para o usuários 
+def escreva():
+    txt = str(input("Insira o texto: "))
+    print (f'{len(txt)*"~"}')
+    print (txt)
+    print (f'{len(txt)*"~"}')
+'''
+
+escreva('Olá mundo!')
---
+++ Python/Exercícios/exercicio098.py
@@ -0,0 +1,10 @@
+import time
+'''
+def contador(ini, fim, passo):
+    while True:
+        c = 1
+        ini passo
+
+'''
+
+print (10 // 3)
