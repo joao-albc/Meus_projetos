c2c9aa553b7391363413620f3f067ddc2fbcd2e5
João Barbosa <joao.albc@gmail.com>
2020-06-08 20:52:34 -0300

    Resolução do exercício 100
    As funções do Guanabara já tinham valor (lista), as minhas ficaram vazias

--- Python/Exercícios/exercicio100.py
+++ Python/Exercícios/exercicio100.py
@@ -1,22 +1,29 @@
-import random
+from random import randint
+from time import sleep
 
 numeros = []
 
 def sorteia():
+    print ('Sorteando os números: ', end=' ')
     for c in range (0,5):
-        num = random.randint(1,100)
-        print (num)
+        num = randint(1,100)
+        print (num, end = ' ')
         numeros.append(num)
+        sleep(0.5)
+    print('Pronto!')
+
 def somaPar():
     par = 0
     for c in numeros:
         if c % 2 == 0:
             par += c
-            print (c,end = ' ')
-    print (f'A soma dos valores pares é {par}')
+            #print (c,end = ' ')
+    print (par)
+    #print (f'A soma dos valores pares é {par}')
+
 
 sorteia()
+print (f'Somando os valores pares de {numeros}, temos: ', end = '')
 somaPar()
-print (numeros)
 
 
